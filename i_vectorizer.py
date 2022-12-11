14b7346312239bc2d3dd7520334a87b3d4ba49ca
Gunardi Ali <gunardilin@live.com>
2022-12-11 22:25:16 +0100

    LDA Visualisation pyLDAvis created

--- i_vectorizer.py
+++ i_vectorizer.py
@@ -12,6 +12,8 @@ from nltk.stem.wordnet import WordNetLemmatizer
 import numpy as np
 import types
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 def tokenize(text:str, language:str="english")-> Generator[str]:
     """Remove affixes, e.g. plurality, -"ing", -"tion", etc.
 
@@ -112,6 +114,7 @@ class GensimVectorizer(BaseEstimator, TransformerMixin):
         self.tfidf = tfidf
         self.tfidf_model = None
         self.documents = None
+        self.model = None
         
     def load(self):
         if os.path.exists(self.path):
@@ -119,6 +122,9 @@ class GensimVectorizer(BaseEstimator, TransformerMixin):
             
     def save(self):
         self.id2word.save(self.path)
+    
+    # def get_feature_names(self):
+    #     return list(self.id2word.values())
         
     def fit(self, documents):
         """
@@ -142,7 +148,7 @@ class GensimVectorizer(BaseEstimator, TransformerMixin):
         "len(self.id2word)" length.
         """
         if self.tfidf:
-            tfidf_model = gensim.models.TfidfModel(dictionary=self.id2word, \
+            self.model = gensim.models.TfidfModel(dictionary=self.id2word, \
                 normalize=True)
         for document in self.documents:
             # Applying frequency vectorization:
@@ -158,6 +164,85 @@ class GensimVectorizer(BaseEstimator, TransformerMixin):
                 docvec = temp
             elif self.tfidf: # Mode3: If True: TF-IDF
                 docvec_temp = docvec
-                docvec = tfidf_model[docvec_temp]
+                docvec = self.model[docvec_temp]
 
             yield sparse2full(docvec, len(self.id2word))
+
+class GensimVectorizer_Topic_Discovery(BaseEstimator, TransformerMixin):
+    """Vectorizing each document when self.transform is executed.
+    Reason for using Gensim: Gensim Dictionary can be saved to disk.
+    It allows reloading the Dictionary without requiring a refit.
+    
+    It could perform either 1) Frequency vectorization or 2) One Hot Encoding
+    or 3) TF-IDF
+    1) Frequency vectorization: binary=False, tfidf=True/False
+    2) One Hot Encoding: binary=True, tfidf=True/False
+    3) TF-IDF: binary=False, tfidf=True
+    By initializing self.binary=True, One Hot Encoding will be used.
+    If self.binary=False, Frequency Vectorization is active. 
+    """
+    def __init__(self, path:str=None, binary=False, tfidf=False):
+        "path: save location for Dictionary after performing self.fit(...)"
+        "Change binary to True to activate One Hot Encoding."
+        self.path = path
+        self.id2word = None
+        self.load()
+        self.binary = binary
+        self.tfidf = tfidf
+        self.tfidf_model = None
+        self.documents = None
+        self.model = None
+        
+    def load(self):
+        if os.path.exists(self.path):
+            self.id2word = Dictionary.load(self.path)
+            
+    def save(self):
+        self.id2word.save(self.path)
+    
+    # def get_feature_names(self):
+    #     return list(self.id2word.values())
+        
+    def fit(self, documents):
+        """
+        This method constructs the Dictionary obj by passing already 
+        tokenized & normalized documents to the Dictionary constructor.
+        Then the Dictionary is saved to disk, so that the transformer
+        can be loaded without requiring a refit.
+        """
+        if isinstance(documents, types.GeneratorType):
+            self.documents = list(documents)
+        else:
+            self.documents = documents
+        self.id2word = Dictionary(self.documents)
+        self.id2word.filter_extremes(no_below=20)
+        self.save()
+        return self
+    
+    def transform(self, documents:list) -> Generator[np.ndarray]:
+        """
+        Transforming document vectors so that every document contains exactly 
+        "len(self.id2word)" length.
+        """
+        if self.tfidf:
+            self.model = gensim.models.TfidfModel(dictionary=self.id2word, \
+                normalize=True)
+        def generator():
+            for document in self.documents:
+                # Applying frequency vectorization:
+                docvec = self.id2word.doc2bow(document) # Mode1: Frequency Vectorization
+                if self.binary: # Mode2: If True: One Hot Encoding.
+                    temp = []
+                    for index_, frequency in docvec:
+                        if frequency > 0:
+                            onehot_freq = 1
+                        else:
+                            onehot_freq = 0
+                        temp.append((index_, onehot_freq))
+                    docvec = temp
+                elif self.tfidf: # Mode3: If True: TF-IDF
+                    docvec_temp = docvec
+                    docvec = self.model[docvec_temp]
+
+                yield docvec
+        return list(generator())
--- o_LDA_scikit.py
+++ o_LDA_scikit.py
@@ -1,14 +1,23 @@
 from sklearn.pipeline import Pipeline
 from h_readsqlite import SqliteCorpusReader
-from i_vectorizer import TextNormalizer, GensimVectorizer
+from i_vectorizer import TextNormalizer, GensimVectorizer, GensimVectorizer_Topic_Discovery
 from sklearn.decomposition import LatentDirichletAllocation, TruncatedSVD, NMF
+
+from gensim.models import LsiModel
+from o_ldamodel import LdaTransformer
+from o_lsimodel import LsiTransformer
 
 import time
 from l_pipeline import timer
 
-class Estimator(object):
+import pyLDAvis
+import pyLDAvis.sklearn
+import pyLDAvis.gensim_models
+
+class SklearnEstimator(object):
     def __init__(self, n_topics=50, estimator="LDA"):
         self.n_topics = n_topics
+        self.documents = None
 
         if estimator == 'LSA':
             self.estimator = TruncatedSVD(n_components=self.n_topics)
@@ -21,10 +30,11 @@ class Estimator(object):
         return self
     
     def transform(self, documents):
-        self.estimator.fit_transform(list(documents))
+        self.documents = list(documents)
+        self.estimator.fit_transform(self.documents)
         
 class SklearnTopicModels(object):
-    def __init__(self, n_topics=50, estimator="LDA", \
+    def __init__(self, n_topics=20, estimator="LDA", \
         db_path:str="DB/StackOverflow.sqlite", \
         gensim_lexicon:str="other/lexicon.pkl"):
         self.n_topics = n_topics
@@ -32,8 +42,9 @@ class SklearnTopicModels(object):
         self.model = Pipeline([
             ("norm", TextNormalizer()),
             ("vect", GensimVectorizer(gensim_lexicon, False, True)),
-            ("model", Estimator(self.n_topics, estimator))
+            ("model", SklearnEstimator(self.n_topics, estimator)),
         ])
+        self.estimator_model = None
     
     def fit_transform(self, year:int):
         docs = self.corpus_reader.docs(year)
@@ -43,12 +54,12 @@ class SklearnTopicModels(object):
     def get_topics(self, n_words=25):
         vectorizer = self.model.named_steps['vect']
         # Get object Estimator
-        model = self.model.steps[-1][1]
+        self.estimator_model = self.model.steps[-1][1]
         # Get a dict with id as index and token as value: 
         names = vectorizer.id2word.id2token
         topics = dict()
         
-        for idx, topic in enumerate(model.estimator.components_):
+        for idx, topic in enumerate(self.estimator_model.estimator.components_):
             # topic is an array of token weights
             # In next line we get the indices that would sort an array.
             # The indices would be starting from token with most weight only
@@ -56,15 +67,91 @@ class SklearnTopicModels(object):
             features = topic.argsort()[:-(n_words-1): -1]
             tokens = [names[i] for i in features]
             topics[idx] = tokens
-            
         return topics
+    
+    def visualize_topic(self):
+        # lda_tf -> self.estimator_model
+        # dtm_tf -> self.model.steps[-1][1].documents
+        # tf_vectorizer -> self.model.steps[-2][1]
+        pyLDAvis.sklearn.prepare(self.estimator_model.estimator,\
+            self.model.steps[-1][1].documents, \
+            self.model.named_steps['vect'])
+        # pyLDAvis.gensim_models.prepare()
+        return
+
+class GensimTopicModels(object):
+    def __init__(self, n_topics=20, estimator="LDA", \
+        db_path:str="DB/StackOverflow.sqlite", \
+        gensim_lexicon:str="other/lexicon.pkl"):
+        self.corpus_reader = SqliteCorpusReader(path=db_path)     
+        self.n_topics = n_topics
+        self.docs = []
+        self.doc_matrix = None
+        self.id2word = None
+
+        if estimator == 'LSA':
+            self.estimator = LsiTransformer(num_topics=self.n_topics)
+        else:
+            self.estimator = LdaTransformer(num_topics=self.n_topics)
+        
+        self.model = Pipeline([
+            ("norm", TextNormalizer()),
+            ("vect", GensimVectorizer_Topic_Discovery(gensim_lexicon, False, True)),
+            # ("model", self.estimator),
+        ])
+    
+    def fit(self, year:int):
+        docs = self.corpus_reader.docs(year)
+        self.docs = list(docs)
+        self.doc_matrix = self.model.fit_transform(self.docs)
+        vectorizer = self.model.named_steps['vect']
+        self.estimator.id2word = vectorizer.id2word.id2token
+        self.estimator.fit(self.doc_matrix)
+        return self.model
+    
+    def get_topics(self, n_words=25):
+        names = self.estimator.id2word
+        topics = dict()
+        for idx, topic in enumerate (self.estimator.gensim_model.get_topics()):
+            features = topic.argsort()[:-(n_words-1): -1]
+            tokens = [names[i] for i in features]
+            topics[idx] = tokens
+        return topics
+    
+    def visualize_topics(self):
+        lda_model = self.estimator.gensim_model
+        corpus = self.doc_matrix
+        lexicon = self.model.named_steps['vect'].id2word
+        data = pyLDAvis.gensim_models.prepare(lda_model, corpus, lexicon)
+        pyLDAvis.save_html(data, 'other/lda.html')
+        return data
+        
+        
 
 if __name__ == "__main__":
+    
+    ## With Sklearn
+    # start_time = time.time()
+    # model = SklearnTopicModels(n_topics=50, estimator="LDA")
+    # model.fit_transform(2022)
+    # topics = model.get_topics()
+    # for topic, term in topics.items():
+    #     print("Topic #{}:".format(topic+1))
+    #     print(term)  
+    # # model.visualize_topic()
+    # timer(start_time, time.time())
+    
+    
+    ## With Gensim
     start_time = time.time()
-    skmodel = SklearnTopicModels(n_topics=50, estimator="NMF")
-    skmodel.fit_transform(2022)
-    topics = skmodel.get_topics()
-    for topic, term in topics.items():
-        print("Topic #{}:".format(topic+1))
-        print(term)  
+    model = GensimTopicModels(n_topics=50, estimator="LDA")
+    model.fit(2021)
+    # print(model.estimator.gensim_model.print_topics(10))
+    topics = model.get_topics()
+    n = 0
+    for topic in topics.values():
+        n += 1
+        print("Topic #{}:".format(n))
+        print(topic)
+    model.visualize_topics()
     timer(start_time, time.time())
