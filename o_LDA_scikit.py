750deed8292b68d7eddb4d7e7e5b23843d1ebfba
Gunardi Ali <61133664+gunardilin@users.noreply.github.com>
2023-01-28 17:52:11 +0100

    Models, Lexicon & DocMatrix are saved in a folder.

--- o_LDA_scikit.py
+++ o_LDA_scikit.py
@@ -29,7 +29,7 @@ import webbrowser
 import platform
 
 import logging
-# logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 # Logging for debug purpose:
 # gensim_logfile_path = 'other/temp/gensim_logs.log'
@@ -107,13 +107,12 @@ class SklearnTopicModels(object):
 class GensimTopicModels(object):
     def __init__(self, n_topics=20, estimator="LDA", \
         db_path:str="DB/StackOverflow.sqlite", \
-        gensim_lexicon:str="other/model/lexicon.pkl",
         eval_every=None,    # Change to 1 when finding optimal passes and itterations parameters
         random_state=None,
         memory_friendly=True, # memory_friendly false necessary for find_optimal_lda_num_topics
         doc_matrix=None,    # Necessary for find_optimal_lda_num_topics
         vectorizer=None):   # Necessary for find_optimal_lda_num_topics
-        self.lexicon_path = gensim_lexicon
+        self.lexicon_path = None
         self.estimator_str = estimator
         self.corpus_reader = SqliteCorpusReader(path=db_path)     
         self.n_topics = n_topics
@@ -123,14 +122,12 @@ class GensimTopicModels(object):
         self.doc_matrix = doc_matrix
         self.vectorizer = vectorizer
         self.id2word = None
-        self.model_folderpath = "other/model"
-        self.model_filepath = "{}/{}_model".format(self.model_folderpath, estimator)
+        self.model = None
+        self.model_folderpath = None#"other/model"
+        self.model_filepath = None#"{}/{}_model".format(self.model_folderpath, estimator)
         self.tempFolderPath = "other/temp"
-        self.doc_matrix_pickle_path = "{}/temp.pickle".format(self.tempFolderPath)
+        self.doc_matrix_pickle_path = None
         self.memory_friendly = memory_friendly
-        if self.memory_friendly:
-            # memory_friendly false is necessary for find_optimal_lda_num_topics
-            self.remove_temp([self.doc_matrix_pickle_path, gensim_lexicon])
 
         if estimator == 'LSA':
             self.estimator = LsiTransformer(num_topics=self.n_topics)
@@ -142,11 +139,6 @@ class GensimTopicModels(object):
             self.estimator = "ensembleLDA"
         # self.load_model()
         
-        self.model = Pipeline([
-            ("norm", TextNormalizer()),
-            ("ngram", NgramVectorizer()),
-            ("vect", GensimVectorizer_Topic_Discovery(gensim_lexicon, False, True)),
-        ])
     
     def save_model(self):
         if not os.path.exists(self.model_folderpath):
@@ -187,6 +179,17 @@ class GensimTopicModels(object):
             self.docs += list(docs)      
         print("Variable size: {}".format(sys.getsizeof(self.docs)))
         del docs                        # Free up memory
+        if self.model == None:
+            self.model_folderpath = "other/model_{}-{}".format(start_year, end_year)
+            self.lexicon_path = "{}/lexicon.pkl".format(self.model_folderpath)
+            if not os.path.exists(self.model_folderpath):
+                os.makedirs(self.model_folderpath)
+            self.model = Pipeline([
+                ("norm", TextNormalizer()),
+                ("ngram", NgramVectorizer()),
+                ("vect", GensimVectorizer_Topic_Discovery(self.lexicon_path, False, True)),
+            ])
+        
         doc_matrix = self.model.fit_transform(self.docs)
         vectorizer = self.model.named_steps['vect']
         if self.memory_friendly:
@@ -198,8 +201,28 @@ class GensimTopicModels(object):
                         vectorizer=None, doc_matrix=None):
         self.start_year = start_year
         self.end_year = end_year
+        
+        self.model_folderpath = "other/model_{}-{}".format(start_year, end_year)
+        self.model_filepath = "{}/{}_model_{}_topics".format(self.model_folderpath,\
+            self.estimator_str, self.n_topics)
+        
+        self.doc_matrix_pickle_path = "{}/doc_matrix.pickle".format(self.model_folderpath)
+        
+        self.lexicon_path = "{}/lexicon.pkl".format(self.model_folderpath)
+        self.model = Pipeline([
+            ("norm", TextNormalizer()),
+            ("ngram", NgramVectorizer()),
+            ("vect", GensimVectorizer_Topic_Discovery(self.lexicon_path, False, True)),
+        ])
+        
         if vectorizer==None or doc_matrix==None:
             print("Extracting from Corpus the doc_matrix and vectorizer.")
+            
+            # Delete existing lexicon  only during first run, when vectorizer 
+            # and doc_matrix are not supplied.
+            if os.path.exists(self.lexicon_path):
+                self.remove_temp([self.lexicon_path])
+            
             self.doc_matrix, self.vectorizer = self.get_Doc_matrix_Vectorizer(\
                 start_year, end_year, limit)
         else:
@@ -207,17 +230,7 @@ class GensimTopicModels(object):
             # This way the whole corpus doesn'need to be reread multiple times.
             self.doc_matrix, self.vectorizer = doc_matrix, vectorizer
             print("Doc_matrix and vectorizer are supplied.")
-        # for year in range(start_year, end_year+1):
-        #     print("Reading corpus from Y{}".format(year))
-        #     docs = self.corpus_reader.docs(year, limit)
-        #     self.docs += list(docs)      
-        # print("Variable size: {}".format(sys.getsizeof(self.docs)))
-        # del docs                        # Free up memory
-        # self.doc_matrix = self.model.fit_transform(self.docs)
-        # vectorizer = self.model.named_steps['vect']
-        # if self.memory_friendly:
-        #     self.docs = None                # Free up memory
-        #     vectorizer.documents = None     # Free up memory
+
         if self.estimator != "ensembleLDA":
             self.estimator.id2word = self.vectorizer.id2word.id2token
             self.estimator.partial_fit(self.doc_matrix)
@@ -251,6 +264,7 @@ class GensimTopicModels(object):
                 epsilon=epsilon,
                 eval_every=eval_every
             )
+        
         self.save_doc_matrix_to_pickle(self.doc_matrix)
         self.doc_matrix = None
         self.save_model()
@@ -299,8 +313,8 @@ class GensimTopicModels(object):
         return topics
     
     def visualize_topics(self, open_in_browser=True):
-        html_path = 'other/lda_{}-{}_{}_topics.html'.format(self.start_year,\
-            self.end_year, self.n_topics)
+        html_path = '{}/lda_{}-{}_{}_topics.html'.format(self.model_folderpath, \
+            self.start_year, self.end_year, self.n_topics)
         if self.estimator_str != "LDA":
             print("** The pyLDAvis is only compatible for LDA not the currently used model.")
             return
@@ -366,7 +380,7 @@ class GensimTopicModels(object):
         return
     
 def find_optimal_lda_num_topics(min=5, max=15, step=1, start_year=2022, end_year=2022, \
-    lexicon_path="other/model/lexicon.pkl", limit=None, random_state=100):
+    limit=None, random_state=100):
     # refer to: 
     # https://rare-technologies.com/what-is-topic-coherence/
     # https://nbviewer.org/github/devashishd12/gensim/blob/280375fe14adea67ce6384ba7eabf362b05e6029/docs/notebooks/topic_coherence_tutorial.ipynb#topic=1&lambda=1&term=
@@ -381,12 +395,7 @@ def find_optimal_lda_num_topics(min=5, max=15, step=1, start_year=2022, end_year
             lexicon.id2token[value] = key
         return lexicon
     
-    if os.path.exists(lexicon_path):
-        lexicon = read_lexicon(lexicon_path)
-    else:
-        lexicon = None
-        # If no existing lexicon exists:
-        # Read lexicon later after being created by model inside the following for loop.
+    lexicon = None
         
     coherence_values = []
     for num_topics in range(min, max+1, step):
@@ -400,6 +409,8 @@ def find_optimal_lda_num_topics(min=5, max=15, step=1, start_year=2022, end_year
         # vectorizer = model.model.named_steps['vect']
 
         if lexicon == None:
+            folder_path = "other/model_{}-{}".format(start_year, end_year)
+            lexicon_path = "{}/lexicon.pkl".format(folder_path)
             lexicon = read_lexicon(lexicon_path)
         
         coherencemodel = CoherenceModel(model=model.estimator.gensim_model, texts=vectorizer.documents, dictionary=lexicon, coherence='c_v')
@@ -410,12 +421,13 @@ def find_optimal_lda_num_topics(min=5, max=15, step=1, start_year=2022, end_year
     
     # show graph
     import matplotlib.pyplot as plt
-    x = range(min, max, step)
+    x = range(min, max+1, step)
     plt.plot(x, coherence_values)
     plt.xlabel("Num Topics")
     plt.ylabel("Coherence score")
-    plt.legend(("coherence_values"), loc='best')
-    plt.savefig("other/convergencegraph_{}-{}.pdf".format(start_year, end_year))
+    plt.legend('', frameon=False)
+    # plt.legend(("coherence_values"), loc='best')
+    plt.savefig("{}/convergencegraph_{}-{}.pdf".format(folder_path, start_year, end_year))
     plt.show()
     
     return
@@ -427,6 +439,7 @@ def find_optimal_lda_num_topics_slow(min=5, max=15, step=1, start_year=2022, end
     # My guess: it is due to not enough memory capacity (8GB) on MacBook Air M2.
        
     def read_lexicon(path):
+        print("Reading lexicon.")
         lexicon = Dictionary.load(path)
         for key, value in lexicon.token2id.items():
             lexicon.id2token[value] = key
@@ -516,5 +529,5 @@ if __name__ == "__main__":
     
     ## Check optimal num topics
     start_time = time.time()
-    find_optimal_lda_num_topics_slow(5,8, 1, 2018, 2021, limit=50)
+    find_optimal_lda_num_topics(5, 8, 1, 2018, 2021, limit=50)
     timer(start_time, time.time())
